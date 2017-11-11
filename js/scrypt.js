b9fd36cad42834e80794b226752b6bda72473dc1
Liudmila Kurkova <kurkova.25051972@gmail.com>
2017-11-11 08:49:35 +0300

    секция about пропадание/появление для 768 на js

---
+++ js/scrypt.js
@@ -0,0 +1,10 @@
+document.getElementById('hamburger-menu-link').onclick = function() {
+      document.getElementById('section-about').style.display = 'none';
+      document.getElementById('dropsection-about').style.display = 'block';
+    }
+
+document.getElementById('img-close').onclick = function() {
+      document.getElementById('dropsection-about').style.display = 'none';
+      document.getElementById('section-about').style.display = 'block';
+    }
+
