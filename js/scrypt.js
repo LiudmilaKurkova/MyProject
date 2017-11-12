6814a7f1019dbd90fbf4ec315c6983f43dc96e3e
Liudmila Kurkova <kurkova.25051972@gmail.com>
2017-11-12 22:58:43 +0300

    секиция burger

--- js/scrypt.js
+++ js/scrypt.js
@@ -1,10 +1,12 @@
 document.getElementById('hamburger-menu-link').onclick = function() {
-      document.getElementById('section-about').style.display = 'none';
-      document.getElementById('dropsection-about').style.display = 'block';
+      document.getElementById('about').style.display = 'none';
+      document.getElementById('drop-about').style.display = 'block';
+      //document.getElementById('drop-about').style.opacity = '1';
     }
 
 document.getElementById('img-close').onclick = function() {
-      document.getElementById('dropsection-about').style.display = 'none';
-      document.getElementById('section-about').style.display = 'block';
+      document.getElementById('drop-about').style.display = 'none';
+      document.getElementById('about').style.display = 'block';
+      //document.getElementById('about').style.opacity = '1';
     }
 
