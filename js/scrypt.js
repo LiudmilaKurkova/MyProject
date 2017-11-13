45584f756bf2811cde4983c717140c78c3cf6212
Liudmila Kurkova <kurkova.25051972@gmail.com>
2017-11-13 16:07:29 +0300

    доработан js для свойства opacity, добавлен z-index, все работает

--- js/scrypt.js
+++ js/scrypt.js
@@ -1,10 +1,12 @@
 document.getElementById('hamburger-menu-link').onclick = function() {
       document.getElementById('about').style.opacity = '0';
       document.getElementById('drop-about').style.opacity = '1';
+      document.getElementById('drop-about').style.zIndex++;
      }
 
 document.getElementById('img-close').onclick = function() {
 			document.getElementById('about').style.opacity = '1';
       document.getElementById('drop-about').style.opacity = '0';
+      document.getElementById('drop-about').style.zIndex--;
       }
 
