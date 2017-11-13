2b9b96a199082e66b455ea813b77cd803cc5c093
Liudmila Kurkova <kurkova.25051972@gmail.com>
2017-11-13 15:27:48 +0300

    Исправлен JS но свойство opacity, работает в одну строну

--- js/scrypt.js
+++ js/scrypt.js
@@ -1,12 +1,10 @@
 document.getElementById('hamburger-menu-link').onclick = function() {
-      document.getElementById('about').style.display = 'none';
-      document.getElementById('drop-about').style.display = 'block';
-      //document.getElementById('drop-about').style.opacity = '1';
-    }
+      document.getElementById('about').style.opacity = '0';
+      document.getElementById('drop-about').style.opacity = '1';
+     }
 
 document.getElementById('img-close').onclick = function() {
-      document.getElementById('drop-about').style.display = 'none';
-      document.getElementById('about').style.display = 'block';
-      //document.getElementById('about').style.opacity = '1';
-    }
+			document.getElementById('about').style.opacity = '1';
+      document.getElementById('drop-about').style.opacity = '0';
+      }
 
