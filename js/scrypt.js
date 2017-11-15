cb6cd4f542a9f9abdd67d78b9d9526b438245bb6
Liudmila Kurkova <kurkova.25051972@gmail.com>
2017-11-15 12:55:54 +0300

    секция аккордеон работает с присваиванием доп класса на js

--- js/scrypt.js
+++ js/scrypt.js
@@ -1,3 +1,4 @@
+//about
 let hamburgerMenu = document.getElementById('hamburger-menu-link');
 let block = document.getElementById('about');
 let buttonClose = document.getElementById('img-close');
@@ -8,10 +9,36 @@ let hiddenMenu = document.getElementById('drop-about');
       block.style.opacity = '0';
       hiddenMenu.style.opacity = '1';
       hiddenMenu.style.zIndex++;
+      hiddenMenu.style.position = 'fixed';
      })
 
 			buttonClose.addEventListener('click', function() {
       block.style.opacity = '1';
       hiddenMenu.style.opacity = '0';
       hiddenMenu.style.zIndex--;
+      hiddenMenu.style.position = 'absolute';
      })
+
+//team
+$(document).ready ( () => {
+
+      $('.main-team__trigger').on('click', (e) => {
+        console.log('ghbdtnm');
+
+  $(e.currentTarget).parent('.main-team__item').toggleClass('main-team__item--activ');
+  
+})
+})
+
+/*
+ $(document).ready(function() {
+
+      $('.main-team__trigger').click( function() {
+
+  $(this).parent('.main-team__item').toggleClass('main-team__item--activ');
+  
+})
+})
+*/
+
+
